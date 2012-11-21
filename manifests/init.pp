8187bb4aba9ebd3bbaf50f0ca1da3ebe32bf6698
cegeka-jenkins <cegeka-jenkins@cegeka.be>
2012-11-21 16:00:37 +0100

    Initial repository layout for puppet module jmxtrans

---
+++ manifests/init.pp
@@ -0,0 +1,16 @@
+# Class: jmxtrans
+#
+# This module manages jmxtrans
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class jmxtrans {
+
+
+}
---
+++ spec/classes/jmxtrans_spec.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env rspec
+
+require 'spec_helper'
+
+describe 'jmxtrans' do
+  it { should contain_class 'jmxtrans' }
+end
